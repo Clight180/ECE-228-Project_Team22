ffcfa8e874b80575f70c7d2b9aa9d36fc4a6703c
Chris <cdlight@ucsd.edu>
2022-05-31 12:10:35 -0700

    13st commit

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ rawImsPath = './raw_images/'
 tensorDataPath = './TensorData/'
 savedModelsPath = './savedModels/'
 modelNum = 000 # Set to pre-existing model to avoid training from epoch 1 , ow 000
-datasetID = 498 # Set to pre-existing dataset to avoid generating a new one, ow 000
+datasetID = 000 # Set to pre-existing dataset to avoid generating a new one, ow 000
 trainSize = 100
 testSize = int(trainSize*.2)
-imDims = 224
+imDims = 128
 numAngles = 12
 showSummary = False
-numWorkers = 4
+
 
 # Hyperparameters:
 num_epochs = 5
-batchSize = 5
+batchSize = 10
 learningRate = 1e-3
 weightDecay = 1e-5
 AMSGRAD = True
